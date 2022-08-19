b0a45a18d995ab9e96b55c8ed81ee7596ad4cc4b
Yong Jun <wdragj@gmail.com>
2022-08-19 23:24:34 +0900

    2022/08/19 2

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, jsonify, json
 import pymysql
+import pprint
+import requests
 
 app = Flask(__name__)
 db = pymysql.connect(host='abandoned-dogs.cdlurfzj5gl4.ap-northeast-2.rds.amazonaws.com', port=3306, user='wdragj',
-                     passwd='dydwnstj71507350', db='test_database', charset="utf8")
+                     passwd='dydwnstj71507350', db='abandoned_dog', charset="utf8")
 cursor = db.cursor()
+
+
+encode = "ieHW%2FCmVoKfe3X9EnL2OT8JoMTqCSRxMT9%2FE5Fr4spuLN4s4Hms5ZiIAZm%2BgvmlMkm06BDRPZHKyrNW4Qo%2F%2B2w%3D%3D"  # 인증키
+url = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20211201&endde=20211231&upkind=417000&pageNo=1&numOfRows=1000&_type=json&serviceKey={}'.format(
+    encode)
+response = request.get(url)
+
+# 데이터 값 출력해보기
+contents = response.text
+
+# 데이터 결과값 예쁘게 출력해주는 코드
+pp = pprint.PrettyPrinter(indent=4)
+print(pp.pprint(contents))
 
 # ------------------- HOME PAGE FUNCTIONS ------------------- #
 
@@ -23,7 +38,7 @@ def abandoned_dog_page():
 
 @app.route("/abandoned-dogs/list", methods=["GET"])
 def abandoned_dog_list():
-    sql = "select breed_no, breed_name_kr from dog_info order by breed_name_kr asc;"
+    sql = "select breed_no, breed_name_kr from breed_info order by breed_name_kr asc;"
     cursor.execute(sql)
     result = cursor.fetchall()
 
@@ -50,7 +65,7 @@ def survey_answer():
     # answer_receive = request.get_json()  # Stores user's survey answer
 
     # Afghan Hound / Affenpinscher
-    sql = "select * from dog_info where dog_breed = 'Afghan Hound';"
+    sql = "select * from breed_info where dog_breed = 'Afghan Hound';"
     cursor.execute(sql)
     result = cursor.fetchall()
     result = result[0]  # Tuple unboxing
